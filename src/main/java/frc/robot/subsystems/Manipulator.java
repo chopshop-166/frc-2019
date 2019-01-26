b20d97ad5fb9fac53debddd22b44d402dc5e96e4
Rohan Sreenivasan <rohan_s885@yahoo.com>
2019-01-26 11:27:12 -0500

    Manipulator commands
    You will see the new commands for the manipulator subsystem. These commands were added to the code from the list in git hub.

---
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -0,0 +1,148 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+public class Manipulator extends Subsystem {
+
+    public Manipulator(final RobotMap map) { // NOPMD
+        super();
+        // Take values that the subsystem needs from the map, and store them in the
+        // class
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
+
+    public Command pickUpBall() {
+        //This command will pick up a ball
+        return new Command("Pick up ball", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command releaseBall() {
+        //This command will release a ball
+        return new Command("Release ball", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    } public Command pickUpHatch() {
+        //This command will pick up a hatch
+        return new Command("Pick up Hatch", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command releaseHatch() {
+        //This command will relase a hatch
+        return new Command("Release a Hatch", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+}
