b8ad6018252d666592474fdc111ca494cbfc2fc9
Unknown <daviddesfosse@gmail.com>
2019-01-26 11:49:19 -0500

    Added all the different functions/commands

---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,437 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+public class Drive extends Subsystem {
+
+    public Drive(final RobotMap map) { // NOPMD
+        super();
+        // Take values that the subsystem needs from the map, and store them in the
+        // class
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
+
+    public Command Drive() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("Drive", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command Turn() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("Turn", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command GoStraight() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("GoStraight", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer  needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command Score() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("Score", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command GoXDistance() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("GoXDistance", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command GoStraight() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("GoStraight", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer  needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command Camera() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("Camera", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command GoStraight() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("GoStraight", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer  needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command DownOffDrop() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("DownOffDrop", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command GoStraight() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("GoStraight", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer  needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command Climb() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("Climb", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command GoStraight() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("GoStraight", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer  needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+    public Command ObjectAvoidance() {
+        // The command is named "Sample Command" and requires this subsystem.
+        return new Command("ObjectOvoidence", this) {
+            @Override
+            protected void initialize() {
+                // Called just before this Command runs the first time
+            }
+
+            @Override
+            protected void execute() {
+                // Called repeatedly when this Command is scheduled to run
+            }
+
+            @Override
+            protected boolean isFinished() {
+                // Make this return true when this Command no longer needs to run execute()
+                return false;
+            }
+
+            @Override
+            protected void end() {
+                // Called once after isFinished returns true
+            }
+
+            // Called when another command which requires one or more of the same
+            // subsystems is scheduled to run
+            @Override
+            protected void interrupted() {
+                end();
+            }
+        };
+    }
+}
