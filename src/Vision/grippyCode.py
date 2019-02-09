6d020bcc783ba3d918278761055bd510b0212754
Sean Trimper <xtron202@yahoo.com>
2019-02-09 17:01:06 -0500

    Found Pairs of rectangles
    First Search for the -15 degree contour and add to first part of pair,
    #then search for -75 degree contour, put in pair, assure the -75 contour is to the right of the -15 degree contour,
    #then assure the -75 contour is the closest contour to the -15,
    #by searching for other -75 contours that are either closer or farther

--- src/Vision/grippyCode.py
+++ src/Vision/grippyCode.py
@@ -21,32 +21,55 @@ import math
 # with cond:
 #     if not notified[0]:
 #         cond.wait()
-def findAngle(M):
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    mU_20 = int(M["mu20"] / M["m00"] - (cX * cX))
-    mU_02 = int(M["mu02"] / M["m00"] - (cY * cY))
-    mU_11 = int(M["mu11"] / M["m00"] - (cX * cY))
-    if mU_20 != mU_02:
-        theta = 0.5 * math.atan((2 * mU_11) / (mU_20 - mU_02))
-    return theta
-        
-def findBetterAngle(frame):
-    rows, cols = frame.shape
-    rect = cv2.minAreaRect(cnt)
-    center = rect[0]
-    angle = rect[2]
-    rot = cv2.getRotationMatrix2D(center, angle-90, 1)
-    print(rot)
-    frame = cv2.warpAffine(frame, rot, (rows,cols))
 
 cap = cv2.VideoCapture(1)
-
 cap.set(10,30)
+
 goalFinder = gripV2.GripiPipeline()
 
+rightAngle = -15
+leftAngle = -75
+
+deadzone = 5
 #frame = cv2.imread('Examples/2.jpg',1)
 #goalFinder.process(cap)
+
+#First Search for the -15 degree contour and add to first part of pair,
+#then search for -75 degree contour, put in pair, assure the -75 contour is to the right of the -15 degree contour,
+#then assure the -75 contour is the closest contour to the -15,
+#by searching for other -75 contours that are either closer or farther
+#DONEEEEEEEE!!!!!!!!
+
+#find center of each pair then identify which pair is closest to image center
+def findPairs(contourList):
+    #right box is -15 degrees
+    rightRectList = []
+    #left box is -75 degrees
+    leftRectList = []
+
+    rectPairList = []
+    for contour in contourList:
+        rectangleBox = cv2.minAreaRect(contour)
+
+        
+        if rectangleBox[2] > rightAngle - deadzone and rectangleBox[2] < rightAngle + deadzone:
+            rightRectList.append(rectangleBox)
+       
+        if rectangleBox[2] > leftAngle - deadzone and rectangleBox[2] < leftAngle + deadzone:
+            leftRectList.append(rectangleBox) 
+
+    for leftRect in leftRectList:
+        pair = (leftRect,None)
+        for rightRect in rightRectList:
+            #0 is center and 0 is x coordinate
+            if rightRect[0][0] > leftRect[0][0]:
+                if pair[1] is None or rightRect[0][0] < pair[1][0][0]:
+                    pair = (leftRect,rightRect)
+        if pair[1] != None:
+            rectPairList.append(pair)
+
+    for pair in rectPairList:
+        print ("rectangle pair coordinates: {}, {}".format(pair[0][0][0],pair[1][0][0]))
 
 while(True):
 
@@ -61,7 +84,7 @@ while(True):
     for contour in goalFinder.find_contours_output:
 
         area=cv2.contourArea(contour)
-        if area>10:
+        if area>1000:
             filteredContours.append(contour)
 
     if len(filteredContours) == 0:
@@ -72,26 +95,26 @@ while(True):
     totalX, totalY = 0, 0
 
     for contour in filteredContours:
-        M = cv2.moments(contour)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        contourArea = cv2.contourArea(contour)
-        
         rect = cv2.minAreaRect(contour)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         cv2.drawContours(frame,[box],0,(0,0,255),2)
 
+        cX,cY = rect[0]
+        cX = int(cX)
+        cY = int(cY)
         theta = rect[2]
-        print ("Give me the angle{}".format(math.degrees(theta)))
-        vX = int(math.cos(theta) * 80 + cX)
-        vY = int(math.sin(theta) * 80 + cY)
-        cv2.line(frame, (cX,cY), (vX,vY), (255,0,255), 2)
+        #print ("center and angles: ({},{}), {}".format(cX,cY, (theta)))
 
-        #rect = cv2.minAreaRect(contour)
-        #box = cv2.boxPoints(rect)
-        #box = np.int0(box)
-        #cv2.drawContours(frame,[box],0,(0,0,255),2)
+        vX = int(math.cos(math.radians(theta)) * 70 + cX)
+        vY = int(math.sin(math.radians(theta)) * 70 + cY)
+        wX = int(math.cos(math.radians(theta)) * -70 + cX)
+        wY = int(math.sin(math.radians(theta)) * -70 + cY)
+        #Draws contour lines based off angles of contours
+        cv2.line(frame, (cX,cY), (vX,vY), (255,0,255), 2)
+        cv2.line(frame, (cX,cY), (wX,wY), (255,0,255), 2)
+
+
 
         totalX += cX
         totalY += cY
@@ -99,10 +122,9 @@ while(True):
         #print("contour points {},{}".format(cX,cY))
         cv2.circle(frame, (cX, cY), 7, (255, 0, 0), -1)
 
-        cv2.drawContours(frame, [contour], -1, (255, 255, 0), 2)
-    #cv2.drawContours(frame, filteredContours, -1, (255, 0, 0), 2)
+        #cv2.drawContours(frame, [contour], -1, (255, 255, 0), 2)
 
-
+    findPairs(filteredContours)
     avgPoint = (int(totalX / len(filteredContours)), int(totalY / len(filteredContours)))
 
     dimensions = frame.shape
