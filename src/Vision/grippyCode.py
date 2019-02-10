2e24f43507e6c0aa00dc14717b6a9dbe66cd96f9
Sean Trimper <xtron202@yahoo.com>
2019-02-10 15:59:12 -0500

    Determines Best Pair of Rectangles

--- src/Vision/grippyCode.py
+++ src/Vision/grippyCode.py
@@ -29,29 +29,32 @@ goalFinder = gripV2.GripiPipeline()
 
 rightAngle = -15
 leftAngle = -75
+deadzone = 15
 
-deadzone = 5
-#frame = cv2.imread('Examples/2.jpg',1)
-#goalFinder.process(cap)
+#...
+def findPairOffset(pair):
+    leftcX = pair[0][0][0]
+    rightcX = pair[1][0][0]
+    pairMidpoint = (leftcX + rightcX) / 2
+    pairOffset = abs(imageMidpoint - pairMidpoint)
+    return pairOffset
 
 #First Search for the -15 degree contour and add to first part of pair,
 #then search for -75 degree contour, put in pair, assure the -75 contour is to the right of the -15 degree contour,
 #then assure the -75 contour is the closest contour to the -15,
 #by searching for other -75 contours that are either closer or farther
 #DONEEEEEEEE!!!!!!!!
-
-#find center of each pair then identify which pair is closest to image center
+#   (find center of each pair then identify which pair is closest to image center)
 def findPairs(contourList):
-    #right box is -15 degrees
     rightRectList = []
-    #left box is -75 degrees
     leftRectList = []
-
     rectPairList = []
+
+    #print ("Number of Rectangles: {}".format(len(contourList)))
+
     for contour in contourList:
         rectangleBox = cv2.minAreaRect(contour)
-
-        
+ 
         if rectangleBox[2] > rightAngle - deadzone and rectangleBox[2] < rightAngle + deadzone:
             rightRectList.append(rectangleBox)
        
@@ -68,33 +71,51 @@ def findPairs(contourList):
         if pair[1] != None:
             rectPairList.append(pair)
 
+    closestCenterPair = None
     for pair in rectPairList:
-        print ("rectangle pair coordinates: {}, {}".format(pair[0][0][0],pair[1][0][0]))
+        print ("rectangle pair coordinates: {}, {}".format(pair[0][0][0],pair[1][0][0]))       
+        if closestCenterPair is None or findPairOffset(pair) < findPairOffset(closestCenterPair):
+            closestCenterPair = pair
+            return closestCenterPair
 
+# MAIN LOOP
 while(True):
-
-
-
+    #capture image
     ret, frame = cap.read()
     cv2.waitKey(300)
 
+    #process with GRIP stuff
     goalFinder.process(frame)
 
+    #filter returned contours
     filteredContours=[]
     for contour in goalFinder.find_contours_output:
-
         area=cv2.contourArea(contour)
         if area>1000:
             filteredContours.append(contour)
 
-    if len(filteredContours) == 0:
-        cv2.imshow('image',frame)
-        continue 
+    #find best pair, if we found contours
+    if len(filteredContours) >= 2:
+        bestPair = findPairs(filteredContours)
+        if bestPair != None:
+            rectangleMidpoint = (int((bestPair[1][0][0] + bestPair[0][0][0]) / 2), int((bestPair[1][0][1] + bestPair[0][0][1]) / 2))
+            cv2.circle(frame, (rectangleMidpoint), 7, (0, 255, 0), -1)
+        avgPoint = (int(totalX / len(filteredContours)), int(totalY / len(filteredContours)))
+
+    #show image!
+    cv2.imshow('image',frame)
+
+cv2.waitKey(0)
+cap.release()
+cv2.destroyAllWindows()
 
 
-    totalX, totalY = 0, 0
+"""
+old method for finding angles
 
-    for contour in filteredContours:
+totalX, totalY = 0, 0
+
+ for contour in filteredContours:
         rect = cv2.minAreaRect(contour)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
@@ -114,8 +135,6 @@ while(True):
         cv2.line(frame, (cX,cY), (vX,vY), (255,0,255), 2)
         cv2.line(frame, (cX,cY), (wX,wY), (255,0,255), 2)
 
-
-
         totalX += cX
         totalY += cY
 
@@ -124,35 +143,7 @@ while(True):
 
         #cv2.drawContours(frame, [contour], -1, (255, 255, 0), 2)
 
-    findPairs(filteredContours)
-    avgPoint = (int(totalX / len(filteredContours)), int(totalY / len(filteredContours)))
-
-    dimensions = frame.shape
-    width = frame.shape[1]
-
-
-
-    #coordinate i have then subtract center in pixels to find midpoint
-    imageMidpoint = (width / 2)
-    avgPointOrient = (avgPoint[0] - imageMidpoint)
-    fullContourMidpoint = (avgPointOrient / imageMidpoint)
-    # prints after ewww math THIS IS THE CONTOUR MIDPOINT from...
-    # Contour Midpoint Scale (-1 to 1)
-    #print('Final Contour Midpoint: ',fullContourMidpoint)
-
-    #table.putNumber("Vision Correction", fullContourMidpoint)
-
-    #print("Average Contour Point {},{}".format(avgPoint[0],avgPoint[1]))
-    cv2.circle(frame, (avgPoint), 7, (255, 255, 255), -1)
-
-    cv2.imshow('image',frame)
-    #filteredContours.clear()
-
-
-cv2.waitKey(0)
-cap.release()
-cv2.destroyAllWindows()
-
+"""
 
 
 
