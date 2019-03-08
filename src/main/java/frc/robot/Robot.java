4d1f67dcb8c52b7f84595d742e71e23774c5dd05
Sean Trimper <xtron202@yahoo.com>
2019-03-08 12:10:44 -0500

    Added Drive Map for Tempest For vision use

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.command.InstantCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.maps.CurrentRobot;
+import frc.robot.maps.Tempest;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Leds;
 import frc.robot.subsystems.LiftSubsystem;
@@ -30,21 +31,21 @@ import frc.robot.subsystems.LiftSubsystem.Heights;
  */
 public class Robot extends CommandRobot {
 
-    final private RobotMap robotMap = new CurrentRobot();
+    final private RobotMap robotMap = new Tempest();
     final public static ButtonXboxController xBoxCoPilot = new ButtonXboxController(1);
     final private Maflipulator maflipulator = new Maflipulator(robotMap.getMaflipulatorMap());
     final private Drive drive = new Drive(robotMap.getDriveMap());
-    final private LiftSubsystem lift = new LiftSubsystem(robotMap.getLiftMap());
+    //final private LiftSubsystem lift = new LiftSubsystem(robotMap.getLiftMap());
     final private Manipulator manipulator = new Manipulator(robotMap.getManipulatorMap());
     public static ButtonXboxController driveController = new ButtonXboxController(5);
     public static Leds leds = new Leds();
     private Command autonomousCommand;
     final private SendableChooser<Command> chooser = new SendableChooser<>();
 
-    private UsbCamera cameraBack;
-    private UsbCamera cameraFront;
-    private VideoSink videoSink;
-    private boolean cameraBackActive = true;
+    // private UsbCamera cameraBack;
+    // private UsbCamera cameraFront;
+    // private VideoSink videoSink;
+    // private boolean cameraBackActive = true;
 
     /**
      * This function is run when the robot is first started up and should be used
@@ -53,25 +54,25 @@ public class Robot extends CommandRobot {
     @Override
     public void robotInit() {
         // Initialize OI here
-        cameraBack = CameraServer.getInstance().startAutomaticCapture(0);
-        cameraFront = CameraServer.getInstance().startAutomaticCapture(1);
-        cameraBack.setResolution(320, 240);
-        cameraFront.setResolution(320, 240);
-        // cameraBack.setFPS(20);
+        // cameraBack = CameraServer.getInstance().startAutomaticCapture(0);
+        // cameraFront = CameraServer.getInstance().startAutomaticCapture(1);
+        // cameraBack.setResolution(320, 240);
+        // cameraFront.setResolution(320, 240);
+        // // cameraBack.setFPS(20);
         // cameraFront.setFPS(20);
         // cameraFront.setExposureAuto();
         // cameraBack.setExposureAuto();
         // videoSink = CameraServer.getInstance().getServer();
-        // videoSink.getProperty("compression").set(30);
-        cameraBack.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
-        cameraFront.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
-        // Initialize autonomous chooser
+        //videoSink.getProperty("compression").set(30);
+        // cameraBack.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+        // cameraFront.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+        // // Initialize autonomous chooser
         // chooser.setDefaultOption("Default Auto", exampleSubsystem.sampleCommand());
         // chooser.addOption("My Auto", new MyAutoCommand());
         // SmartDashboard.putData("Auto mode", chooser);
         // SmartDashboard.putData("Switch Cameras", switchCameras());
-        SmartDashboard.putData("Good Flip", goodFlip());
-        SmartDashboard.putData("Darken Cameras", darkenCameras());
+//        SmartDashboard.putData("Good Flip", goodFlip());
+        // SmartDashboard.putData("Darken Cameras", darkenCameras());
         assignButtons();
     }
 
@@ -118,56 +119,56 @@ public class Robot extends CommandRobot {
     // });
     // }
 
-    public Command goodFlip() {
-        CommandChain retValue = new CommandChain("Good Flip");
+    // public Command goodFlip() {
+    //     CommandChain retValue = new CommandChain("Good Flip");
 
-        retValue.then(lift.goToAtLeast(LiftSubsystem.Heights.kLiftFlipHeight)).then(maflipulator.crappyFlip());
-        return retValue;
-    }
+    //     retValue.then(lift.goToAtLeast(LiftSubsystem.Heights.kLiftFlipHeight)).then(maflipulator.crappyFlip());
+    //     return retValue;
+    // }
 
-    public Command stowAndGo() {
-        CommandChain retValue = new CommandChain("Stow it and go!");
-        retValue.then(lift.goToHeight(Heights.kFloorLoad), maflipulator.stowAndGoPosition());
-        return retValue;
-    }
+    // public Command stowAndGo() {
+    //     CommandChain retValue = new CommandChain("Stow it and go!");
+    //     retValue.then(lift.goToHeight(Heights.kFloorLoad), maflipulator.stowAndGoPosition());
+    //     return retValue;
+    // }
 
-    public Command levelOne() {
-        CommandChain retValue = new CommandChain("Level one");
+    // public Command levelOne() {
+    //     CommandChain retValue = new CommandChain("Level one");
 
-        retValue.then(lift.goToHeight(Heights.kLoadingStation));
-        return retValue;
+    //     retValue.then(lift.goToHeight(Heights.kLoadingStation));
+    //     return retValue;
 
-    }
+    // }
 
-    public Command levelTwo() {
-        CommandChain retValue = new CommandChain("Level 2");
+    // public Command levelTwo() {
+    //     CommandChain retValue = new CommandChain("Level 2");
 
-        retValue.then(lift.goToHeight(Heights.kRocketHatchMid), maflipulator.goToScoringPosition());
-        return retValue;
+    //     retValue.then(lift.goToHeight(Heights.kRocketHatchMid), maflipulator.goToScoringPosition());
+    //     return retValue;
 
-    }
+    // }
 
-    public Command levelThree() {
-        CommandChain retValue = new CommandChain("Level 3");
+    // public Command levelThree() {
+    //     CommandChain retValue = new CommandChain("Level 3");
 
-        retValue.then(lift.goToHeight(Heights.kRocketHatchHigh), maflipulator.goToScoringPosition());
-        return retValue;
+    //     retValue.then(lift.goToHeight(Heights.kRocketHatchHigh), maflipulator.goToScoringPosition());
+    //     return retValue;
 
-    }
+    // }
 
-    public Command darkenCameras() {
-        return new InstantCommand(() -> {
-            // cameraBack.setBrightness(0);
-            cameraFront.setBrightness(0);
-        });
-    }
+    // public Command darkenCameras() {
+    //     return new InstantCommand(() -> {
+    //         // cameraBack.setBrightness(0);
+    //         cameraFront.setBrightness(0);
+    //     });
+    // }
 
-    public Command brightenCameras() {
-        return new InstantCommand(() -> {
-            // cameraBack.setExposureAuto();
-            cameraFront.setExposureAuto();
-        });
-    }
+    // public Command brightenCameras() {
+    //     return new InstantCommand(() -> {
+    //         //cameraBack.setExposureAuto();
+    //         cameraFront.setExposureAuto();
+    //     });
+    // }
 
     public void assignButtons() {
         xBoxCoPilot.getButton(ButtonXboxController.XBoxButton.BUMPER_LEFT).whenPressed(manipulator.openBeak());
---
+++ src/main/java/frc/robot/maps/Tempest.java
@@ -0,0 +1,145 @@
+package frc.robot.maps;
+
+import com.chopshop166.chopshoplib.outputs.MockSpeedController;
+import com.chopshop166.chopshoplib.outputs.SendableSpeedController;
+import com.chopshop166.chopshoplib.sensors.Lidar;
+import com.chopshop166.chopshoplib.sensors.MockPotentiometer;
+import com.chopshop166.chopshoplib.sensors.PIDGyro;
+import com.chopshop166.chopshoplib.sensors.SparkMaxCounter;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import frc.robot.RobotMap;
+
+public class Tempest implements RobotMap {
+
+    @Override
+    public LiftMap getLiftMap() {
+        return new LiftMap() {
+
+            @Override
+            public DigitalInput getUpperLimit() {
+                return new DigitalInput(4);
+            }
+
+            @Override
+            public CANSparkMax getMotor() {
+                // return new MockSpeedController();
+                return null;
+            }
+
+            @Override
+            public DigitalInput getLowerLimit() {
+                return new DigitalInput(0);
+            }
+
+            @Override
+            public SparkMaxCounter getHeightEncoder() {
+                return null;
+            }
+
+            @Override
+            public DoubleSolenoid getBrake() {
+                return new DoubleSolenoid(2, 7);
+            }
+        };
+    }
+
+    @Override
+    public ManipulatorMap getManipulatorMap() {
+        return new ManipulatorMap() {
+
+            @Override
+            public SendableSpeedController getrollersMotor() {
+                return new MockSpeedController();
+            }
+
+            @Override
+            public SendableSpeedController getpivotPointsMotor() {
+                return new MockSpeedController();
+            }
+
+            @Override
+            public DigitalInput getintakePositionLimitSwitch() {
+                return new DigitalInput(1);
+            }
+
+            @Override
+            public DigitalInput getfoldedBackLimitSwitch() {
+                return new DigitalInput(2);
+            }
+
+            @Override
+            public DoubleSolenoid getbeaksPiston() {
+                return new DoubleSolenoid(2, 3);
+            }
+
+            @Override
+            public DigitalInput getGamepieceLimitSwitch() {
+                return new DigitalInput(3);
+            }
+        };
+    }
+
+    @Override
+    public DriveMap getDriveMap() {
+        return new DriveMap() {
+
+            @Override
+            public Lidar getLidar() {
+                return new Lidar(Port.kOnboard, 0x10);
+            }
+
+            @Override
+            public SendableSpeedController getRight() {
+                return SendableSpeedController.wrap(new SpeedControllerGroup(new WPI_TalonSRX(2), new WPI_TalonSRX(3)));
+            }
+
+            @Override
+            public SendableSpeedController getLeft() {
+                return SendableSpeedController.wrap(new SpeedControllerGroup(new WPI_TalonSRX(4), new WPI_TalonSRX(1)));
+            }
+
+            @Override
+            public PIDGyro getGyro() {
+                return PIDGyro.mock();
+            }
+
+            @Override
+            public Encoder getLeftEncoder() {
+                return new Encoder(6, 7);
+            }
+
+            @Override
+            public DoubleSolenoid getClimbPiston() {
+                return new DoubleSolenoid(4, 5);
+            }
+
+            @Override
+            public Encoder getRightEncoder() {
+                return new Encoder(9, 8);
+            }
+        };
+    }
+
+    public MaflipulatorMap getMaflipulatorMap() {
+        return new MaflipulatorMap() {
+
+            @Override
+            public SendableSpeedController getFlipMotor() {
+                return SendableSpeedController.wrap(new WPI_TalonSRX(5));
+            }
+
+            @Override
+            public Potentiometer getMaflipulatorPot() {
+                return new MockPotentiometer();
+            }
+        };
+    }
+}
