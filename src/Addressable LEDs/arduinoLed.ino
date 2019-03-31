010062337c651bcbc7d2a316e6896f51b2490178
Sean Trimper <xtron202@yahoo.com>
2019-03-31 09:20:17 -0400

    Meteor LEDs

--- src/Addressable LEDs/arduinoLed.ino
+++ src/Addressable LEDs/arduinoLed.ino
@@ -10,41 +10,62 @@ void setup()
 
 void loop()
 {
-    CylonBounce(0, 0, 0xff, 6, 8, 30);
+    meteorRain(0xff, 0xff, 0xff, 10, 64, true, 35);
 }
 
-void CylonBounce(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay)
+void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTrailDecay, boolean meteorRandomDecay, int SpeedDelay)
 {
+    setAll(0, 0, 255);
 
-    for (int i = 0; i < NUM_LEDS - EyeSize - 2; i++)
+    for (int i = 0; i < NUM_LEDS + NUM_LEDS; i++)
     {
-        setAll(255, 255, 255);
-        setPixel(i, red / 10, green / 10, blue / 10);
-        for (int j = 1; j <= EyeSize; j++)
+
+        // fade brightness all LEDs one step
+        for (int j = 0; j < NUM_LEDS; j++)
         {
-            setPixel(i + j, red, green, blue);
+            if ((!meteorRandomDecay) || (random(10) > 5))
+            {
+                fadeToBlack(j, meteorTrailDecay);
+            }
         }
-        setPixel(i + EyeSize + 1, red / 10, green / 10, blue / 10);
+
+        // draw meteor
+        for (int j = 0; j < meteorSize; j++)
+        {
+            if ((i - j < NUM_LEDS) && (i - j >= 0))
+            {
+                setPixel(i - j, red, green, blue);
+            }
+        }
+
         showStrip();
         delay(SpeedDelay);
     }
+}
 
-    delay(ReturnDelay);
+void fadeToBlack(int ledNo, byte fadeValue)
+{
+#ifdef ADAFRUIT_NEOPIXEL_H
+    // NeoPixel
+    uint32_t oldColor;
+    uint8_t r, g, b;
+    int value;
 
-    for (int i = NUM_LEDS - EyeSize - 2; i > 0; i--)
-    {
-        setAll(255, 255, 255);
-        setPixel(i, red / 10, green / 10, blue / 10);
-        for (int j = 1; j <= EyeSize; j++)
-        {
-            setPixel(i + j, red, green, blue);
-        }
-        setPixel(i + EyeSize + 1, red / 10, green / 10, blue / 10);
-        showStrip();
-        delay(SpeedDelay);
-    }
+    oldColor = strip.getPixelColor(ledNo);
+    r = (oldColor & 0x00ff0000UL) >> 16;
+    g = (oldColor & 0x0000ff00UL) >> 8;
+    b = (oldColor & 0x000000ffUL);
 
-    delay(ReturnDelay);
+    r = (r <= 10) ? 0 : (int)r - (r * fadeValue / 256);
+    g = (g <= 10) ? 0 : (int)g - (g * fadeValue / 256);
+    b = (b <= 10) ? 0 : (int)b - (b * fadeValue / 256);
+
+    strip.setPixelColor(ledNo, r, g, b);
+#endif
+#ifndef ADAFRUIT_NEOPIXEL_H
+    // FastLED
+    leds[ledNo].fadeToBlackBy(fadeValue);
+#endif
 }
 void showStrip()
 {
