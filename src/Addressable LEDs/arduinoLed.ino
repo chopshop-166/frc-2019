b46321b98f64912a2a0ba28b78df7ba5927cb0e8
Sean Trimper <xtron202@yahoo.com>
2019-04-23 23:06:13 -0400

    Added more effects for leds

--- src/Addressable LEDs/arduinoLed.ino
+++ src/Addressable LEDs/arduinoLed.ino
@@ -10,12 +10,106 @@ void setup()
 
 void loop()
 {
-
+    // NewKITT(0, 0, 0xff, 8, 10, 50);
     colorWipe(0x00, 0x00, 0xff, 20);
     colorWipe(0x00, 0x00, 0x00, 20);
     // meteorRain(0xff, 0xff, 0xff, 10, 64, true, 15);
-    //Fire(30, 200, 0);
+    // Fire(30, 200, 0);
     // rainbowCycle(5);
+    // RunningLights(0xff, 0xff, 0xff, 50);
+    // theaterChase(0, 0, 0xff, 50);
+}
+
+void NewKITT(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay)
+{
+    OutsideToCenter(red, green, blue, EyeSize, SpeedDelay, ReturnDelay);
+    CenterToOutside(red, green, blue, EyeSize, SpeedDelay, ReturnDelay);
+}
+
+void CenterToOutside(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay)
+{
+    for (int i = ((NUM_LEDS - EyeSize) / 2); i >= 0; i--)
+    {
+        setAll(0, 0, 0);
+
+        setPixel(i, red / 10, green / 10, blue / 10);
+        for (int j = 1; j <= EyeSize; j++)
+        {
+            setPixel(i + j, red, green, blue);
+        }
+        setPixel(i + EyeSize + 1, red / 10, green / 10, blue / 10);
+
+        setPixel(NUM_LEDS - i, red / 10, green / 10, blue / 10);
+        for (int j = 1; j <= EyeSize; j++)
+        {
+            setPixel(NUM_LEDS - i - j, red, green, blue);
+        }
+        setPixel(NUM_LEDS - i - EyeSize - 1, red / 10, green / 10, blue / 10);
+
+        showStrip();
+        delay(SpeedDelay);
+    }
+    delay(ReturnDelay);
+}
+
+void OutsideToCenter(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay)
+{
+    for (int i = 0; i <= ((NUM_LEDS - EyeSize) / 2); i++)
+    {
+        setAll(0, 0, 0);
+
+        setPixel(i, red / 10, green / 10, blue / 10);
+        for (int j = 1; j <= EyeSize; j++)
+        {
+            setPixel(i + j, red, green, blue);
+        }
+        setPixel(i + EyeSize + 1, red / 10, green / 10, blue / 10);
+
+        setPixel(NUM_LEDS - i, red / 10, green / 10, blue / 10);
+        for (int j = 1; j <= EyeSize; j++)
+        {
+            setPixel(NUM_LEDS - i - j, red, green, blue);
+        }
+        setPixel(NUM_LEDS - i - EyeSize - 1, red / 10, green / 10, blue / 10);
+
+        showStrip();
+        delay(SpeedDelay);
+    }
+    delay(ReturnDelay);
+}
+
+void LeftToRight(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay)
+{
+    for (int i = 0; i < NUM_LEDS - EyeSize - 2; i++)
+    {
+        setAll(0, 0, 0);
+        setPixel(i, red / 10, green / 10, blue / 10);
+        for (int j = 1; j <= EyeSize; j++)
+        {
+            setPixel(i + j, red, green, blue);
+        }
+        setPixel(i + EyeSize + 1, red / 10, green / 10, blue / 10);
+        showStrip();
+        delay(SpeedDelay);
+    }
+    delay(ReturnDelay);
+}
+
+void RightToLeft(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay)
+{
+    for (int i = NUM_LEDS - EyeSize - 2; i > 0; i--)
+    {
+        setAll(0, 0, 0);
+        setPixel(i, red / 10, green / 10, blue / 10);
+        for (int j = 1; j <= EyeSize; j++)
+        {
+            setPixel(i + j, red, green, blue);
+        }
+        setPixel(i + EyeSize + 1, red / 10, green / 10, blue / 10);
+        showStrip();
+        delay(SpeedDelay);
+    }
+    delay(ReturnDelay);
 }
 void colorWipe(byte red, byte green, byte blue, int SpeedDelay)
 {
